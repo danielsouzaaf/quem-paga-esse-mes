e9262a0af09345f02c9464a32ea8655e8f46b22b
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2019-05-31 21:45:15 -0300

    adicionando um background para ficar mais bonitin

--- src/components/Pagantes.vue
+++ src/components/Pagantes.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pagantes">
-    <div class="inline-block relative w-64">
+    <div class="inline-block relative w-64 mt-5">
       <select v-model="servico" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
         <option value="netflix">Netflix</option>
         <option value="spotify">Spotify</option>
@@ -24,7 +24,7 @@
           </div>
         </div>
       </div>
-      <div class="mt-5">
+      <div class="mt-5 text-xl">
         <p class="title">Pagantes do {{servico}}</p>
         <div class="lists">
           <ul class="nes-list is-disc">
